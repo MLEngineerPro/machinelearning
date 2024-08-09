e46331c543257f53051a5fc2d3b7e096c7017926
MLEngineerPro <9873551+MLEngineerPro@users.noreply.github.com>
2024-08-09 08:32:24 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ import joblib
 st.title('🎈 Machine Learning App')
 st.info('This app build a machine learning model')
 st.text('Fixed width text')
+
+st.select_slider('Selecciones la edad', options=[1,'2'])
